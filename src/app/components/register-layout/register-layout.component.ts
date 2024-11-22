c4e20ceda566b66bf6d8038931acaf3f00dfc83d
Pedro <peecampossobrinho@gmail.com>
2024-11-22 00:10:26 -0400

    AreaVermelha

---
+++ src/app/components/register-layout/register-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './register-layout.component.html',
+  styleUrl: './register-layout.component.scss'
+})
+export class RegisterLayoutComponent {
+
+}
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RegisterLayoutComponent } from '../../components/register-layout/register-layout.component';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    RegisterLayoutComponent
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
